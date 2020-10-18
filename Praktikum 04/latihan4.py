58a342e3f93721e08fa6da78979917db373e0362
Nur Falah Abdurrozaq <nufa@student.uns.ac.id>
2020-10-18 20:33:33 +0700

    Update latihan4.py

--- Praktikum 04/latihan4.py
+++ Praktikum 04/latihan4.py
@@ -9,11 +9,9 @@ istirahat = 45
 waktu1 = jarak1/kecepatan1
 waktu2 = jarak2/kecepatan2
 
-waktu_menit = ((waktu1+waktu2) * 60) + istirahat
-jam = int(waktu_menit // 60)
+waktu_menit = ((waktu1+waktu2) * 60) + istirahat + menit_berangkat
+jam = int(waktu_menit // 60) + jam_berangkat
 menit = int(waktu_menit % 60)
 
-jam_sampai = jam_berangkat + jam
-menit_sampai = menit_berangkat + menit
 print('\nWaktu berangkat pukul {:02}.{:02}'.format(jam_berangkat, menit_berangkat))
-print('Pak Amir akan sampai di kota C pada pukul {:02}.{:02}'.format(jam_sampai, menit_sampai))
+print('Pak Amir akan sampai di kota C pada pukul {:02}.{:02}'.format(jam, menit))
