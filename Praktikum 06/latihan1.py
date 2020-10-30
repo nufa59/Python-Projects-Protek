1cea32e58658890ae91f196c4feec1f3cd6e6f43
Nur Falah Abdurrozaq <nufa@student.uns.ac.id>
2020-10-30 15:41:05 +0700

    update

--- Praktikum 06/latihan1.py
+++ Praktikum 06/latihan1.py
@@ -1,8 +1,7 @@
 def isPythagoras(a, b, c):
     return c**2 == a**2 + b**2
 
-nilai_a = int(input('Masukan nilai a: '))
-nilai_b = int(input('Masukan nilai b: '))
-nilai_c = int(input('Masukan nilai c: '))
-
-print(f'a = {nilai_a}, b = {nilai_b}, c = nilai_{nilai_c}   ->',isPythagoras(nilai_a, nilai_b, nilai_c))
+print(f'a = 3, b = 4, c = 5   -> {isPythagoras(3, 4, 5)}')
+print(f'a = 5, b = 9, c = 12  -> {isPythagoras(5, 9, 12)}')
+print(f'a = 8, b = 6, c = 10  -> {isPythagoras(8, 6, 10)}')
+print(f'a = 7, b = 8, c = 11  -> {isPythagoras(7, 8, 11)}')
